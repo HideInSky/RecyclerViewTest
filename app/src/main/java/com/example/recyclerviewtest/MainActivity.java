47c4575e996ec8bf2e20dda96a27925024a44bc8
linyucheng112 <linyucheng112@qq.com>
2021-03-20 01:28:25 +0800

    1. add item_design.xml 2. add ModelClass.java 3. add Adapter.java 4. revise MainActivity.java finish this test

--- app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.recyclerviewtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    List<ModelClass> userList;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        initRecyclerView();
+
+
+    }
+
+    private void initData() {
+
+        userList = new ArrayList<>();
+        userList.add(new ModelClass(R.drawable.avocado, "avocado", "__"));
+        userList.add(new ModelClass(R.drawable.blueberry, "blueberry", "__"));
+        userList.add(new ModelClass(R.drawable.cherry, "cherry", "__"));
+        userList.add(new ModelClass(R.drawable.mango, "mango", "__"));
+
+
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new Adapter(userList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 }
